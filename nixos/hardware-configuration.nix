4d53c584414fc1b456d295d37fb0ed5f1c661544
Nandonirv <108136513+Nandonirv@users.noreply.github.com>
2024-10-21 12:54:01 +0200

    Update hardware-configuration.nix

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -10,36 +10,26 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/79e630e2-199b-4a46-989e-7795d9300300";
+    { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0C15-2455";
+    { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
+      options = ["umask=0077"];
     };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/14b98e5a-2027-4e36-bcb8-0a368d0e7361"; }
-    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  nix.settings.max-jobs = 16;
-  nix.extraOptions = ''
-    build-cores = 20
-  '';
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
