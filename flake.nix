479f9c3e4c478f2e9be1892d663d8bc06782dbaa
Nandonirv <108136513+Nandonirv@users.noreply.github.com>
2024-10-21 20:30:58 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -30,13 +30,13 @@
         }
 
        # Enable Cosmic DE
-       {
-         nix.settings = {
-           substituters = [ "https://cosmic.cachix.org/" ];
-           trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-         };
-       }
-       nixos-cosmic.nixosModules.default
+       #{
+        # nix.settings = {
+         #  substituters = [ "https://cosmic.cachix.org/" ];
+          # trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+       #  };
+     #  }
+      # nixos-cosmic.nixosModules.default
             
       ];
     };
