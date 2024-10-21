b8f583e67a4257872ee9e21d0a57d5615875c48d
Nandonirv <108136513+Nandonirv@users.noreply.github.com>
2024-10-21 12:43:23 +0200

    Add files via upload

---
+++ nixos/configuration.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, ... }:
+
+{
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.kernelModules = [ "kvm-amd" ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = false;
+  
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Networking
+  networking.hostName = "officepc";
+  networking.networkmanager.enable = true;
+
+  # Nvidia GPU Settings
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.open = true;
+
+  # Time zone & Locale
+  time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Keymap
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "";
+  };
+  console.keyMap = "fr";
+
+  # Users
+  users.users.matt = {
+    initialPassword = "password123";
+    isNormalUser = true;
+    description = "Matt Knowles";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  # Packages
+  environment.systemPackages = with pkgs; [  
+    git
+  ];
+  
+  # Enable Greeter
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
+  
+  # System state Version
+  system.stateVersion = "24.05";
+
+}
