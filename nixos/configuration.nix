8348e74dcade650131c6b59c21a728b94f9c536e
Nandonirv <108136513+Nandonirv@users.noreply.github.com>
2024-10-21 12:48:01 +0200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,23 +1,10 @@
 { config, pkgs, ... }:
 
 {
-
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.kernelModules = [ "kvm-amd" ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/boot";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  nixpkgs.hostPlatform = "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = false;
+  imports =
+    [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
   
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
